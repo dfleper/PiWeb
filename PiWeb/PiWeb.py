27233e23baba089fa1625844b94c56cec3e2b8f5
dfleper <doflepe@gmail.com>
2025-05-11 13:46:03 +0100

    Login Design

--- PiWeb/PiWeb.py
+++ PiWeb/PiWeb.py
@@ -1,29 +1,93 @@
 import reflex as rx
 from rxconfig import config
 
-
 class State(rx.State):
     """The app state."""
-
     ...
 
-
-def index() -> rx.Component:
-    return rx.container(
-        rx.color_mode.button(position="top-right"),
-        rx.vstack(
-            rx.heading("Bienvenido...", size="9"),
-            rx.text(
-                "Comienza aquí para editar... ",
-                rx.code(f"{config.app_name}/{config.app_name}.py"),
-                size="5",
+def login_default() -> rx.Component:
+    return rx.center( 
+        rx.card(
+            rx.vstack(
+                rx.center(
+                    rx.image(
+                        src="/logo.png",
+                        width="7em",
+                        height="auto",
+                        border_radius="25%",
+                    ),
+                    rx.heading(
+                        "Sign in to your account",
+                        size="6",
+                        as_="h2",
+                        text_align="center",
+                        width="100%",
+                    ),
+                    direction="column",
+                    spacing="5",
+                    width="100%",
+                ),
+                rx.vstack(
+                    rx.text(
+                        "Email address",
+                        size="3",
+                        weight="medium",
+                        text_align="left",
+                        width="100%",
+                    ),
+                    rx.input(
+                        placeholder="user@email.com",
+                        type="email",
+                        size="3",
+                        width="100%",
+                    ),
+                    justify="start",
+                    spacing="2",
+                    width="100%",
+                ),
+                rx.vstack(
+                    rx.hstack(
+                        rx.text(
+                            "Password",
+                            size="3",
+                            weight="medium",
+                        ),
+                        rx.link(
+                            "Forgot password?",
+                            href="#",
+                            size="3",
+                        ),
+                        justify="between",
+                        width="100%",
+                    ),
+                    rx.input(
+                        placeholder="Enter your password",
+                        type="password",
+                        size="3",
+                        width="100%",
+                    ),
+                    spacing="2",
+                    width="100%",
+                ),
+                rx.button("Sign in", size="3", width="100%"),
+                rx.center(
+                    rx.text("New here?", size="3"),
+                    rx.link("Sign up", href="#", size="3"),
+                    opacity="0.8",
+                    spacing="2",
+                    direction="row",
+                ),
+                spacing="6",
+                width="100%",
             ),
-            spacing="5",
-            justify="center",
-            min_height="85vh",
+            size="4",
+            max_width="28em",
+            width="100%",
         ),
+        height="100vh", 
+        width="100vw",  
     )
 
-
 app = rx.App()
-app.add_page(index)
+app.add_page(login_default, route="/", title="Login | Piweb")
+
